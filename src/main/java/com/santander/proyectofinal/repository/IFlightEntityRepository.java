3d66cdf824237e72ce2575b442779cbc23a3ff17
hersantander <herromero@santandertecnologia.com.ar>
2022-05-30 17:40:26 -0300

    Changes by A446493

--- src/main/java/com/santander/proyectofinal/repository/IFlightEntityRepository.java
+++ src/main/java/com/santander/proyectofinal/repository/IFlightEntityRepository.java
@@ -1,6 +1,7 @@
 package com.santander.proyectofinal.repository;
 
 import com.santander.proyectofinal.entity.FlightEntity;
+import com.santander.proyectofinal.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
