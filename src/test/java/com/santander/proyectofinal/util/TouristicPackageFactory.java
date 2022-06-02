06a7fc0eea1bfef15b12c6adee6e241d77d39b6e
braiSantec <bcontrera@santandertecnologia.com.ar>
2022-06-02 11:30:43 -0300

    commit para traer factories necesarios para package

--- src/test/java/com/santander/proyectofinal/util/TouristicPackageFactory.java
+++ src/test/java/com/santander/proyectofinal/util/TouristicPackageFactory.java
@@ -18,29 +18,34 @@ public class TouristicPackageFactory {
     private boolean isActive;
     private LocalDate createdAt;
     private Double totalAmount;
-    public static TouristicPackageEntity newTouristicPackageEntity(){
-        UserEntity user = new UserEntity(1,"team","123","admin");
+
+    public static TouristicPackageEntity newTouristicPackageEntity() {
+        UserEntity user = new UserEntity(1, "team", "123", "admin");
+
         List<TouristicPackageBookingEntity> touristicPackageBookings = new ArrayList<>();
         List<TouristicPackageReservationEntity> touristicPackagesReservations = new ArrayList<>();
-        TouristicPackageEntity touristicPackageEntity = new TouristicPackageEntity(1,123,"paquete turistico",LocalDate.of(2022,03,12),user,touristicPackageBookings,touristicPackagesReservations);
-        List<PersonEntity> personEntities = new ArrayList<>();
         List<FlightReservationEntity> flightReservationEntities = new ArrayList<>();
-        FlightEntity flightEntity = new FlightEntity(1,"123", LocalDate.of(2022, 03, 12), LocalDate.of(2022, 04, 12),1,"lugano","pompeya","premium",flightReservationEntities);
-        FlightReservationEntity flightReservationEntity = new FlightReservationEntity(1,"pepe"
-                ,LocalDate.of(2022, 03, 12),LocalDate.of(2022, 04, 12),"lugano","pompeya",2,"premium",        FlightEntity flightEntity = new FlightEntity(1,"123", LocalDate.of(2022, 03, 12), LocalDate.of(2022, 04, 12)
-                ,1,"lugano","pompeya","premium",flightReservationEntities,);
-)
 
-        personEntities.add(new PersonEntity(1,"38059243","pepe","mail@algo.com","pepito", LocalDate.of(2022, 03, 12),));
+
+        TouristicPackageEntity touristicPackageEntity = new TouristicPackageEntity(1, 123, "paquete turistico", LocalDate.of(2022, 03, 12), user, touristicPackageBookings, touristicPackagesReservations);
+
+        FlightEntity flightEntity = FlightEntityFactory.newFlightEntity();
+
+
         HotelBookingEntity hotelBooking = new HotelBookingEntity("team", 1, LocalDate.of(2022, 03, 12), LocalDate.of(2022, 04, 12), "lugano", new HotelEntity(), 2, "double", );
 
-        touristicPackageBookings.add(new TouristicPackageBookingEntity(1,touristicPackageEntity,hotelBooking));
-       return new TouristicPackageEntity(1,123,"paquete turistico",LocalDate.of(2022,03,12),user,touristicPackageBookings,touristicPackagesReservations);
+        touristicPackageBookings.add(new TouristicPackageBookingEntity(1, touristicPackageEntity, hotelBooking));
+        return new TouristicPackageEntity(1, 123, "paquete turistico", LocalDate.of(2022, 03, 12), user, touristicPackageBookings, touristicPackagesReservations);
     }
 
-    private  List<TouristicPackageBookingEntity>  refillBookings(){
+    private List<TouristicPackageBookingEntity> refillBookings() {
         List<TouristicPackageBookingEntity> touristicPackageBookings = new ArrayList<>();
-        touristicPackageBookings.add(new TouristicPackageBookingEntity(1,new TouristicPackageEntity(1,123,),new HotelBookingEntity));
+        touristicPackageBookings.add(new TouristicPackageBookingEntity(1, new TouristicPackageEntity(1, 123, ), new HotelBookingEntity));
+    }
+
+    private List<PersonEntity> refillPeople() {
+        List<PersonEntity> people = new ArrayList<>();
+        people.add();
     }
 }
 
