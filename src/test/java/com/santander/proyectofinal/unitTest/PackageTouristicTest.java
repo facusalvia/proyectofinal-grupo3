02bd013ae926aae90ea076db1cd5c52791bccf53
braiSantec <bcontrera@santandertecnologia.com.ar>
2022-06-02 12:03:54 -0300

    se agrega factori touristicPackage

--- src/test/java/com/santander/proyectofinal/unitTest/PackageTouristicTest.java
+++ src/test/java/com/santander/proyectofinal/unitTest/PackageTouristicTest.java
@@ -1,21 +1,12 @@
 package com.santander.proyectofinal.unitTest;
-import com.santander.proyectofinal.dto.request.HotelRequestDTO;
-import com.santander.proyectofinal.entity.HotelEntity;
 import com.santander.proyectofinal.repository.ITouristicPackageRepository;
 import com.santander.proyectofinal.service.TouristicPackageService;
-import com.santander.proyectofinal.util.HotelEntityFactory;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class PackageTouristicTest {
@@ -26,18 +17,9 @@ public class PackageTouristicTest {
     TouristicPackageService touristicPackageService;
 
     @Test
-    void shouldReturnAnAddedHotel(){
+    void shouldReturnAnAddePackageTouristic(){
 
-        HotelEntity hotelEntity = HotelEntityFactory.newHotelEntity();
 
-        HotelRequestDTO hotelToAdd= HotelEntityFactory.newHotelRequestDTO();
-        HotelEntity mockedHotel = HotelEntityFactory.newHotelEntity();
-
-        when(hotelRepository.findByHotelCode(hotelToAdd.getHotelCode())).thenReturn(Optional.empty());
-        when(hotelRepository.save(any())).thenReturn(mockedHotel);
-
-        HotelRequestDTO addedHotel = hotelService.addHotel(hotelToAdd);
-        assertAll(()-> assertEquals(hotelToAdd,addedHotel));
 
     }
 
--- src/test/java/com/santander/proyectofinal/util/TouristicPackageFactory.java
+++ src/test/java/com/santander/proyectofinal/util/TouristicPackageFactory.java
@@ -21,22 +21,8 @@ public class TouristicPackageFactory {
 
     public static TouristicPackageEntity newTouristicPackageEntity() {
         UserEntity user = new UserEntity(1, "team", "123", "admin");
-        List<PersonEntity> people = refillPeople();
-        List<TouristicPackageBookingEntity> touristicPackageBookings = refillBookings();
-        List<TouristicPackageReservationEntity> touristicPackagesReservations = new ArrayList<>();
-        List<FlightReservationEntity> flightReservationEntities = new ArrayList<>();
-        TouristicPackageEntity touristicPackage =
+        return buildTouristicPackageEntity(user);
 
-                TouristicPackageEntity
-        touristicPackageEntity = new TouristicPackageEntity(1, 123, "paquete turistico", LocalDate.of(2022, 03, 12), user, touristicPackageBookings, touristicPackagesReservations);
-
-        FlightEntity flightEntity = FlightEntityFactory.newFlightEntity();
-
-
-        HotelBookingEntity hotelBooking = new HotelBookingEntity("team", 1, LocalDate.of(2022, 03, 12), LocalDate.of(2022, 04, 12), "lugano", new HotelEntity(), 2, "double", );
-
-        touristicPackageBookings.add(new TouristicPackageBookingEntity(1, touristicPackageEntity, hotelBooking));
-        return touristicPackage;
     }
 
     private static List<TouristicPackageBookingEntity> refillBookings(TouristicPackageEntity touristicPackageEntity) {
@@ -48,16 +34,11 @@ public class TouristicPackageFactory {
 
     private static List<TouristicPackageReservationEntity> refillReservations(TouristicPackageEntity touristicPackageEntity) {
         List<TouristicPackageReservationEntity> touristicPackageReservationEntities = new ArrayList<>();
-        touristicPackageReservationEntities.add(new TouristicPackageReservationEntity(1, touristicPackageEntity, FlightReservationFactory.));
+        touristicPackageReservationEntities.add(new TouristicPackageReservationEntity(1, touristicPackageEntity, FlightReservationFactory.newFlightReservationEntity()));
         return touristicPackageReservationEntities;
     }
 
-    private static List<PersonEntity> refillPeople() {
-        List<PersonEntity> people = new ArrayList<>();
-        people.add(PersonEntityFactory.newPersonEntity());
-        people.add(PersonEntityFactory.newPersonEntity());
-        return people;
-    }
+
 
     private static TouristicPackageEntity buildTouristicPackageEntity(UserEntity user) {
         TouristicPackageEntity touristicPackageEntity = new TouristicPackageEntity();
@@ -65,6 +46,7 @@ public class TouristicPackageFactory {
         touristicPackageEntity.setUser(user);
         touristicPackageEntity.setTouristicPackageBookings(refillBookings(touristicPackageEntity));
         touristicPackageEntity.setTouristicPackageReservations(refillReservations(touristicPackageEntity));
+        touristicPackageEntity.setId(1);
         return touristicPackageEntity;
     }
 }
