680734aee36c2b656df140b611d4064b810f5905
MarianaRodriguez1 <marianarodriguez1@santandertecnologia.com.ar>
2022-06-02 15:13:02 -0300

    applicationpropertiesTest agregados de variables

--- src/test/resources/sql/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO hotels VALUES(1,'2022-04-10','2022-04-15','CH-0002',false,'Cataratas Hotel','Puerto Iguazú',6300.0,'Single')
-INSERT INTO hotels VALUES(2,'2022-04-15','2022-04-20','CH-0003',false,'Cataratas Hotel','Puerto Iguazú',6300.0,'Single')
-INSERT INTO hotels VALUES(3,'2022-02-10','2022-03-19','HB-001',false,'Hotel Bristol','Buenos Aires',5435.0,'Single')
-INSERT INTO hotels VALUES(4,'2022-02-08','2022-04-17','BH-002',false,'Hotel Bristol 2','Buenos Aires',7200.0,'Doble')
-INSERT INTO hotels VALUES(5,'2022-04-17','2022-05-23','SH-002',false,'Sheraton','Tucuman',5790.0,'Doble')
-
