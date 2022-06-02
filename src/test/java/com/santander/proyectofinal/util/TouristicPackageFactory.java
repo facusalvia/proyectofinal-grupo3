8dfeb4becdcbe64acd24d4c85ab984602ddc4235
braiSantec <bcontrera@santandertecnologia.com.ar>
2022-06-02 12:20:26 -0300

    se agrega metodo return DTO dentro de factory PackageTouristic

--- src/test/java/com/santander/proyectofinal/util/TouristicPackageFactory.java
+++ src/test/java/com/santander/proyectofinal/util/TouristicPackageFactory.java
@@ -1,4 +1,6 @@
 package com.santander.proyectofinal.util;
+import com.santander.proyectofinal.dto.response.TouristicPackageInfoResponseDTO;
+import com.santander.proyectofinal.dto.response.TouristicPackageResponseDTO;
 import com.santander.proyectofinal.entity.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +11,10 @@ public class TouristicPackageFactory {
         UserEntity user = new UserEntity(1, "team", "123", "admin");
         return buildTouristicPackageEntity(user);
 
+    }
+
+    public static TouristicPackageResponseDTO newTouristicPackageDTO() {
+        return buildTouristicPackageDTO();
     }
 
     private static List<TouristicPackageBookingEntity> refillBookings(TouristicPackageEntity touristicPackageEntity) {
@@ -34,6 +40,21 @@ public class TouristicPackageFactory {
         touristicPackageEntity.setTouristicPackageReservations(refillReservations(touristicPackageEntity));
         touristicPackageEntity.setId(1);
         return touristicPackageEntity;
+    }
+
+    private static TouristicPackageResponseDTO buildTouristicPackageDTO() {
+        TouristicPackageResponseDTO touristicPackageResponseDTO = new TouristicPackageResponseDTO();
+        touristicPackageResponseDTO.setTouristicPackageInfoResponseDTO(new TouristicPackageInfoResponseDTO());
+        return touristicPackageResponseDTO;
+    }
+
+    private static TouristicPackageInfoResponseDTO buildTouristicPackageDTOInfo() {
+        TouristicPackageInfoResponseDTO touristicPackageInfoResponseDTO = new TouristicPackageInfoResponseDTO();
+        touristicPackageInfoResponseDTO.setPackageNumber(123);
+        touristicPackageInfoResponseDTO.setId(1);
+        touristicPackageInfoResponseDTO.setName("paquete");
+        touristicPackageInfoResponseDTO.setClientId(1);
+        return touristicPackageInfoResponseDTO;
     }
 }
 
