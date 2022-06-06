02124128e24a42e95bfc0558454dddb3c3e727f5
braiSantec <bcontrera@santandertecnologia.com.ar>
2022-06-06 04:27:11 -0300

    se agrega metodo newRespDto dentro de ClientEntityFactory

--- src/test/java/com/santander/proyectofinal/util/ClientEntityFactory.java
+++ src/test/java/com/santander/proyectofinal/util/ClientEntityFactory.java
@@ -1,11 +1,17 @@
 package com.santander.proyectofinal.util;
 
+import com.santander.proyectofinal.dto.response.ClientRespDTO;
 import com.santander.proyectofinal.entity.ClientEntity;
 
 public class ClientEntityFactory {
 
     public static ClientEntity newClientEntity(){
-        ClientEntity client = new ClientEntity(1, "team", "juan", "carlos", null, null);
+        ClientEntity client = new ClientEntity(1, "nor", "norberto", "bernardo", null, null);
+        return client;
+    }
+
+    public static ClientRespDTO newClientRespDTO(){
+        ClientRespDTO client = new ClientRespDTO("norberto", "bernardo");
         return client;
     }
 }
