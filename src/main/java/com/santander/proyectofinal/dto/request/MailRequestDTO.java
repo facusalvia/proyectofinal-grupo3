8e71157a39222a34c46af8601d9020aa0e8b1743
braiSantec <bcontrera@santandertecnologia.com.ar>
2022-06-06 02:33:12 -0300

    refactor MailRequestDTO

--- src/main/java/com/santander/proyectofinal/dto/request/MailRequestDTO.java
+++ src/main/java/com/santander/proyectofinal/dto/request/MailRequestDTO.java
@@ -4,13 +4,29 @@ package com.santander.proyectofinal.dto.request;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+@Valid
+@Validated
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class MailRequestDTO {
-    private String mailSubject;
-    private String mailTo;
-    private String mailContent;
-    private String mailFrom;
+    private String subject;
+    @NotBlank(message = "The field to cannot be empty")
+    @NotEmpty
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Email(message = "Insert correct mail to")
+    private String to;
+    private String content;
+    @NotBlank(message = "The field from cannot be empty")
+    @NotEmpty
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Email(message = "Insert correct mail from")
+    private String from;
 }
