ce1322a12d6d31643f8e5693a8167eee66e3a334
braiSantec <bcontrera@santandertecnologia.com.ar>
2022-06-06 04:14:18 -0300

    se agrega metodo en ClientService para obtener el cliente con mas reservas de vuelos

--- src/main/java/com/santander/proyectofinal/service/ClientService.java
+++ src/main/java/com/santander/proyectofinal/service/ClientService.java
@@ -1,8 +1,11 @@
 package com.santander.proyectofinal.service;
 
+import com.santander.proyectofinal.dto.response.ClientRespDTO;
+import com.santander.proyectofinal.dto.response.ClientResponseDTO;
 import com.santander.proyectofinal.dto.response.TopClientsResponseDTO;
 import com.santander.proyectofinal.entity.ClientEntity;
 import com.santander.proyectofinal.repository.IClientRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,11 +17,19 @@ public class ClientService {
 
     @Autowired
     IClientRepository clientRepository;
+   ModelMapper modelMapper = new ModelMapper();
 
     public TopClientsResponseDTO getTopClients(Integer year, int limit) {
         List<Map<ClientEntity,Integer>> clients =  clientRepository.findCLientsTop3();
         TopClientsResponseDTO topClientsResponseDTO = new TopClientsResponseDTO();
 
         return new TopClientsResponseDTO();
+    }
+
+
+    public ClientRespDTO getUserWithMoreFlightReservation() {
+        ClientEntity clients =  clientRepository.findClientWithMoreFlightReservation();
+        ClientRespDTO clientResponseDTO = modelMapper.map(clients, ClientRespDTO.class);
+        return clientResponseDTO;
     }
 }
