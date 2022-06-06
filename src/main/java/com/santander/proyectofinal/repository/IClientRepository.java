f37815b4cbea533a077c97f366629aff88a38c20
braiSantec <bcontrera@santandertecnologia.com.ar>
2022-06-06 04:15:18 -0300

    Se agrega metodo con query native para obtener el cliente con mas reservas de vuelos

--- src/main/java/com/santander/proyectofinal/repository/IClientRepository.java
+++ src/main/java/com/santander/proyectofinal/repository/IClientRepository.java
@@ -1,14 +1,9 @@
 package com.santander.proyectofinal.repository;
 
 import com.santander.proyectofinal.entity.ClientEntity;
-import com.santander.proyectofinal.entity.HotelEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.time.LocalDate;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -18,4 +13,9 @@ public interface IClientRepository extends JpaRepository<ClientEntity, Integer> 
     Optional<ClientEntity> findByUsernameEquals(String username);
     @Query("SELECT new map(c as cli,SUM(c.bookings.size)+SUM(c.reservations.size) as total )FROM ClientEntity as c group by c.id order by total desc ")
     List<Map<ClientEntity,Integer>> findCLientsTop3();
+
+    @Query(value = "Select id,username,name,lastname,max(max) maximo FROM (SELECT cl.id id,cl.username username,cl.name name,cl.lastname lastname,count(f.client_id) max FROM clients cl JOIN flights_reservation f ON f.client_id=cl.id group by f.client_id) a",nativeQuery = true)
+    ClientEntity findClientWithMoreFlightReservation();
+
+
 }
