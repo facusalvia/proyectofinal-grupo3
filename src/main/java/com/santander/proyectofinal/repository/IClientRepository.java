333f544aa665ae9f0ca4a5d478d4d4e42ca1d350
braiSantec <bcontrera@santandertecnologia.com.ar>
2022-06-06 05:01:49 -0300

    se agrega query findClientWithMoreFlightBookings dentro de IClientRepository

--- src/main/java/com/santander/proyectofinal/repository/IClientRepository.java
+++ src/main/java/com/santander/proyectofinal/repository/IClientRepository.java
@@ -14,8 +14,11 @@ public interface IClientRepository extends JpaRepository<ClientEntity, Integer> 
     @Query("SELECT new map(c as cli,SUM(c.bookings.size)+SUM(c.reservations.size) as total )FROM ClientEntity as c group by c.id order by total desc ")
     List<Map<ClientEntity,Integer>> findCLientsTop3();
 
-    @Query(value = "Select id,username,name,lastname,max(max) maximo FROM (SELECT cl.id id,cl.username username,cl.name name,cl.lastname lastname,count(f.client_id) max FROM clients cl JOIN flights_reservation f ON f.client_id=cl.id group by f.client_id) a",nativeQuery = true)
+    @Query(value = "SELECT c.id,c.username,c.name,c.lastname,count(f.client_id) max FROM clients c JOIN flights_reservation f ON f.client_id=c.id group by f.client_id order by max desc limit 1",nativeQuery = true)
     ClientEntity findClientWithMoreFlightReservation();
+
+    @Query(value = "SELECT c.id,c.username,c.name name,c.lastname lastname,count(h.client_id) max FROM clients c JOIN hotel_bookings h ON h.client_id=c.id group by h.client_id order by max desc limit 1",nativeQuery = true)
+    ClientEntity  findClientWithMoreFlightBookings();
 
 
 }
